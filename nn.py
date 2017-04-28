b1ca055a117df35e1fc3e1921bec66d0d153b30f
dennis <oddstar78@gmail.com>
2017-04-28 09:04:42 +0800

    fix bug

--- nn.py
+++ nn.py
@@ -1,8 +1,8 @@
 import numpy as np
-
+import copy
 
 '''
-   激励函数 
+   激励函数
 '''
 def tanh(x):
     return 1 / (1 + np.power(np.e,-1 * x))
@@ -34,8 +34,9 @@ class NeuralNetwork:
         self.activation = tanh
         self.activation_deriv = tanh_deriv
 
-        self.weights = weight
-        self.bias = bias
+
+        self.weights = copy.copy(weight)
+        self.bias = copy.copy(bias)
 
         for i in range(len(layers) - 1):
             if(len(weight) == 0):
@@ -51,43 +52,41 @@ class NeuralNetwork:
     def fit(self,x,y_,learing_rate=0.5, epochs=100):
         # print("--------------fit---------")
         for i in range(epochs):
+            etotal = 0
             for j in range(len(x)):
                 a = [x[j]]
                 for w in range(len(self.weights)):
                     tmp = self.activation(
-                        np.dot(a[w], self.weights[w]) + self.bias[w]
+                        np.dot(a[-1], self.weights[w]) + self.bias[w]
                     )
                     a.append(tmp.tolist())
                 # print_array(a,"output")
                 err = np.array(a[-1]) - np.array(y_[j])
-                etotal = np.std(err)
-                deltas = [np.multiply(err,self.activation_deriv(np.array(a[-1])))]
+                etotal += np.sqrt(np.power(err,2))
+                # print(etotal)
+                deltas = [err * self.activation_deriv(np.array(a[-1]))]
 
                 for w in range(len(self.weights) - 1,0,-1):
                     # print("idx:%s" % w)
                     deltas.append(
-                        deltas[-1].dot(np.array(self.weights[w]).T) * self.activation_deriv(np.array(a[w]))
+                        np.array(deltas[-1]).dot(np.array(self.weights[w]).T) * tanh_deriv(np.array(a[w]))
                      )
-                    # print("del:%s  w:%s   a%s:%s  a%s:%s" % (deltas[-1],np.array(self.weights[idx]).T,idx,a[idx],w,a[w]))
-                    # deltas.append(
-                    #     deltas[-1].dot(self.weights[w].T) * self.activation_deriv(a[w])
-                    #     deltas[-1].dot(np.array(a[idx]).T) * self.activation_deriv(np.array(a[w]))
-                    # )
-                    # print("deltas:%s" % deltas[len(deltas) - 1])
                 deltas.reverse()
                 # print_array(deltas,"deltas")
 
                 # print("----update weight---")
-                for w in range(len(self.weights)):
+                for w in range(len(self.weights)-1,-1,-1):
+                    layer = np.atleast_2d(a[w])
+                    delta = np.atleast_2d(deltas[w])
                     # print("a:%s  deltas:%s  w:%s" % (a[w],deltas[w],self.weights[w]))
-                    # print("s:%s" % np.multiply(a[w],deltas[w]))
-                    self.weights[w] -= learing_rate * np.multiply(a[w],deltas[w])
+                    # self.weights[w] -= learing_rate * np.multiply(deltas[w],dba)
+                    self.weights[w] -= learing_rate * layer.T.dot(delta)
                     self.bias[w] -= learing_rate * deltas[w]
                     # print("new_w:%s" % self.weights[w])
                     # print("-----")
 
                 # print_Weight(self.weights,self.bias)
-                print("etotal:%s" % etotal)
+            print("etotal:%s" % (etotal/len(x)))
 
 
             # print("-----check------")
@@ -104,32 +103,35 @@ class NeuralNetwork:
 
 
 if __name__ == '__main__':
-    nn = NeuralNetwork([2,2,2],
-                       [
-                           [
-                               [0.15,0.25],
-                               [0.2, 0.3]
-                           ],
-                           [
-                               [0.4, 0.5],
-                               [0.45, 0.55]
-                           ],
-                       ],
-                       [
-                           [0.35,0.35],
-                           [0.6, 0.6],
-                       ])
-    x = [
-        [0.05,0.1]
-    ]
-    y = [
-        [0.01,0.99]
-    ]
-    # for i in range(1):
-    #     a = np.random.random()
-    #     b = np.random.random()
-    #     x.append([a,b])
-    #     y.append(a+b)
+    # nn = NeuralNetwork([2,2,2],
+    #                    [
+    #                        [
+    #                            [0.15,0.25],
+    #                            [0.2, 0.3]
+    #                        ],
+    #                        [
+    #                            [0.4, 0.5],
+    #                            [0.45, 0.55]
+    #                        ],
+    #                    ],
+    #                    [
+    #                        [0.35,0.35],
+    #                        [0.6, 0.6],
+    #                    ])
+    # x = [
+    #     [0.05,0.1]
+    # ]
+    # y = [
+    #     [0.01,0.99]
+    # ]
+    nn = NeuralNetwork([2,4,1])
+    x = []
+    y = []
+    for i in range(100):
+        a = np.random.random()
+        b = np.random.random()
+        x.append([a,b])
+        y.append(a+b)
     nn.fit(x,y)
     # nn.fit(
     #     [
